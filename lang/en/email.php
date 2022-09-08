2f715cd3aa0314f1ace3fe70dfeebfd92bde5339
skeleton567 <mikkelletoo@gmail.com>
2022-09-08 18:03:59 +0400

    feat: add email translation

--- lang/en/email.php
+++ lang/en/email.php
@@ -1,7 +1,13 @@
 <?php
 
 return [
-    'subject_verify'=>'Vefify Email',
-    'subject_reset'=>'Reset Password'
+    'subject_verify'=>'Confirm Email',
+    'subject_reset'=>'Reset Password',
+    'click_button' => 'Click this button to',
+    'verify_email' => 'confirm your email',
+    'recover_password' => 'recover password',
+    'button_recover' => 'Recover password',
+    'button_confirm' => 'Confirm email',
+    'confirmation_email' => 'Confirmation email'
 
 ];
